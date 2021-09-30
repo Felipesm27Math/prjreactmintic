7404d450e4c13fbbf1d1f29fd2854a87b1ebdfdc
Yulissa Lopez <lyulissa5@gmail.com>
2021-09-29 21:20:15 -0500

    the component menu was created and  the folder component as well, the styles of the component were ass in the App.css file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/menu';
 
 function App() {
   return (
     <div className="App">
+      <Menu/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>This is our applicaton - reactjs App</h1>
