3188c1387b5b3058402c57648ee30afd14e216b5
Yulissa Lopez <lyulissa5@gmail.com>
2021-09-30 15:00:37 -0500

    create the product register page and its styles on product-register folde, the folder pages was deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 //import logo from './logo.svg';
 //import './App.css';
-
 import Login from "./components/login_oauth2/Login";
 
 function App() {
   return (
     <div className="App">
-     
       <Login />
     </div>
   );
