ae6437e021514b89b935fa615dc634dba76be787
Felipesm27 <felipesm27@gmail.com>
2021-10-02 14:07:34 -0500

    Modificación de interfaz login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 //import logo from './logo.svg';
 //import './App.css';
 import Login from "./components/login_oauth2/Login";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {} from '@fortawesome/free-solid-svg-icons'
+import {} from '@fortawesome/fontawesome-svg-core'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
   return (
--- src/components/login_oauth2/Login.js
+++ src/components/login_oauth2/Login.js
@@ -4,7 +4,8 @@ import './style.css'
 function Login() {
   return (
     <div>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"/>
+      <div id = "login">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"/>
         <h3 class="text-center text-dark">Sign in to Project cycle 3</h3>
         <div class="container">
           <div class="row">
@@ -44,9 +45,10 @@ function Login() {
         </div>
         <div class="createAccount">
           <p>
-            New in this project?<a href="www.google.com">Create an account</a>
+            Continue with<a href="www.google.com"><i class="fab fa-google"></i></a>
           </p>
         </div>
+      </div>
       </div>
   );
 }
