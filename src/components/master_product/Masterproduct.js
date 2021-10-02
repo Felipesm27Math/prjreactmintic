1461c48f90bd8148084c5c3d11c0cb2edbe61e25
Andres Lopez <andres.lop0909@gmail.com>
2021-10-01 22:58:00 -0500

    Creacion interfaz master product con estilos

---
+++ src/components/master_product/Masterproduct.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Menu from '../menu'
+
+/*importar estilos*/ 
+import './style.css'
+
+const Masterproduct = () => {
+    return (
+        <>
+          <Menu/>  
+          <section className="cont-principal">
+              <div className="funciones">
+                  <button type="submit"><i class="fas fa-plus"></i> New product</button>  
+                  <div className="busqueda">
+                      <input type="text"/>
+                      <i class="fas fa-search"></i>
+                  </div>
+              </div>
+              <div className="tabla">
+                  <table>
+                      <thead>
+                          <tr>
+                              <th>id product</th>
+                              <th>Description</th>
+                              <th>Price</th>
+                              <th>Estado</th>
+                              <th>Editar</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          <tr>
+                              <td>Coca cola</td>
+                              <td>bebida</td>
+                              <td>2500</td>
+                              <td>en stock</td>
+                              <td>icono</td>
+                          </tr>
+                      </tbody>
+                  </table>
+              </div>
+          </section>
+        </>
+    )
+}
+
+export default Masterproduct
