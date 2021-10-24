db4f0801c03ebc9bbd7556e4db45edf882bba9b4
Yulissa Lopez <lyulissa5@gmail.com>
2021-10-24 00:22:07 -0500

    creacion formulario nuevo usuario como ventana modal

--- src/components/user_master/Usermaster.js
+++ src/components/user_master/Usermaster.js
@@ -1,13 +1,23 @@
 import React, {useEffect, useState} from 'react';
+import { useRef} from 'react';
 import { obtenerUsuarios } from '../../utils/api';
 import { nanoid } from 'nanoid';
+import Tooltip from '@mui/material/Tooltip';
 import Navbar from '../navbar/Navbar';
 import './style.css';
 
 const Usermaster = () => {
 
     const [user, setUser] = useState([]);
-    const [ejecutarConsulta, setEjecutarConsulta] = useState(true)
+    const [ejecutarConsulta, setEjecutarConsulta] = useState(true);
+    const [showTabla, setShowTabla] = useState(true);
+    const [showForm, setShowForm] = useState(false);
+    const [nameBtn, setNameBtn]= useState("Agregar Usuario")
+
+    const Toggle = ()=>{
+        setShowForm(!showForm)
+        console.log(showForm)
+    }
 
     useEffect(()=>{
        if(ejecutarConsulta){
@@ -25,21 +35,33 @@ const Usermaster = () => {
        
     }, [ejecutarConsulta])
 
-
+    useEffect(()=>{
+        if(showTabla){
+            console.log()
+            setNameBtn("Agregar Usuario")
+        }else{
+            setNameBtn("Mostrar Usuarios")
+        }
+    }, [showTabla])
 
     return (
         <>
           <Navbar/> 
           <section className="block__main">
+            <div className="cont-btn">
+                <button onClick={ 
+                    ()=>{
+                        setShowTabla(!showTabla)
+                        Toggle()
+                    }}
+                className="btn_crear">{nameBtn}</button>
+            </div>
+            <FormNuevo showForm={showForm} close={Toggle}/>
             <div className="block__busqueda">
                 <h4>Que usuario deseas editar?</h4>
-                <select>
-                    <option>Administrador</option>
-                    <option>Vendedor</option>
-                </select>
             </div>
             <div className="block__tabla">
-                <Tabla listaUser ={user}/>
+                    <Tabla listaUser ={user}/>
             </div>
             </section> 
         </>
@@ -82,8 +104,48 @@ const FilaTabla = ({listaVendedores})=>{
             <td>{listaVendedores.email}</td>
             <td>{listaVendedores.state}</td>
             <td>{listaVendedores.rol}</td>
-            <td><i class="far fa-edit"></i></td>
+            <td className="actions_style">
+                <Tooltip title="Editar Usuario" arrow>
+                    <i className="far fa-edit"></i>
+                </Tooltip>
+                <Tooltip title="Eliminar Usuario" arrow>
+                    <i className="far fa-trash-alt"></i>
+                </Tooltip>
+            </td>
+            
         </tr>
+    )
+}
+
+const FormNuevo = ({showForm, close})=>{
+    const formRef = useRef(null);
+
+    const submitForm = ()=>{
+        //codigo para enviar formulario
+    }
+
+    return(
+        <>
+        {
+            showForm ? (
+
+            <div className="formCont" onClick={()=>{close()}}>
+                <div onClick={(e) => e.stopPropagation()} className="formUser">
+                    <form onSubmit={submitForm} ref={formRef} className="fromularioUsuarios">
+                        <h2>Añadir Nuevo Usuario</h2>
+                        <label htmlFor="user">
+                            usuario
+                            <input type="text" name="user" placeholder="nombreUsuario" required/>
+                        </label>
+                    </form>
+                </div>
+            </div>
+            
+            
+            ) : null
+
+        }
+        </>
     )
 }
 
